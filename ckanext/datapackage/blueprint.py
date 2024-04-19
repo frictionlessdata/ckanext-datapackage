1766bdc6c4027d7cb5f3d9043c1b86d54d09c132
roll <eskarev@gmail.com>
2024-04-19 11:49:06 +0100

    Initial endpoint implementation

--- ckanext/datapackage/blueprint.py
+++ ckanext/datapackage/blueprint.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from ckan.plugins import toolkit
+from dplib.plugins.ckan.models import CkanPackage
 from flask import Blueprint, jsonify
 
 datapackage_blueprint = Blueprint(
@@ -8,5 +10,12 @@ datapackage_blueprint = Blueprint(
 
 
 @datapackage_blueprint.route("/dataset/<id>/datapackage.json", methods=["GET"])
-def read_dataset(id):
-    return jsonify({"id": id})
+def read_dataset(id, package_type=None):
+    try:
+        dataset = toolkit.get_action("datapackage_dataset_show")({}, {"id": id})
+        package = CkanPackage.from_dict(dataset).to_dp()
+        return jsonify(package.to_dict())
+    except toolkit.ObjectNotFound:
+        toolkit.abort(404)
+    except Exception:
+        toolkit.abort(500)
