a3fb802cb0f419e16bfe85b48245d437df4b7806
roll <eskarev@gmail.com>
2024-04-19 14:01:16 +0100

    Fixed tests

--- ckanext/datapackage/blueprint.py
+++ ckanext/datapackage/blueprint.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from ckan.plugins import toolkit
-from dplib.models import Model
 from dplib.plugins.ckan.models import CkanPackage
 from flask import Blueprint, jsonify
 
